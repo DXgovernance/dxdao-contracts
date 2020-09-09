bb464ae20b8f9dd1bd0ee69bde2a90dd60f9f827
AugustoL <me@augustol.com>
2020-09-09 16:59:18 -0300

    feat(erc20guild): Add IERC20Guild contract

---
+++ contracts/erc20guild/IERC20Guild.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.5.17;
+pragma experimental ABIEncoderV2;
+
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+
+/// @title IERC20Guild - DRAFT
+/// @author github:AugustoL
+/// @notice This smart contract has not be audited
+/// @dev ERC20Guild Interface
+interface IERC20Guild {
+
+    event ProposalCreated(bytes32 indexed proposalId);
+    event ProposalExecuted(bytes32 indexed proposalId);
+    event VoteAdded(bytes32 indexed proposalId, address voter, uint256 tokens);
+    event VoteRemoved(bytes32 indexed proposalId, address voter, uint256 tokens);
+    
+    function setConfig(
+        uint256 _minimumProposalTime,
+        uint256 _votesForExecution,
+        uint256 _votesForCreation
+    ) external;
+    function createProposal(
+        address[] calldata _to,
+        bytes[] calldata _data,
+        uint256[] calldata _value,
+        string calldata _description,
+        bytes calldata _contentHash,
+        uint256 _extraTime
+    ) external;
+    function executeProposal(bytes32 proposalId) external;
+    function setVote(bytes32 proposalId, uint256 amount) external;
+    function setVotes(bytes32[] calldata proposalIds, uint256[] calldata amounts) external;
+    
+    function votesOf(address account) external view returns(uint256);
+    function votesOf(address[] calldata accounts) external view returns(uint256[] memory) ;
+    function token() external view returns (address);
+    function initialized() external view returns (bool);
+    function nonce() external view returns (uint256);
+    function minimumProposalTime() external view returns (uint256);
+    function votesForExecution() external view returns (uint256);
+    function votesForCreation() external view returns (uint256);
+    function getProposal(bytes32 proposalId) external view returns(
+        address creator,
+        uint256 startTime,
+        uint256 endTime,
+        address[] memory to,
+        bytes[] memory data,
+        uint256[] memory value,
+        string memory description,
+        bytes memory contentHash,
+        uint256 totalVotes,
+        bool executed
+    );
+    function getProposalVotes(bytes32 proposalId, address voter) external view returns(uint256);
+
+}
