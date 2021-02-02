a04e5fd4e783eb05307ac2dda13cc899c4e4c397
AugustoL <me@augustol.com>
2021-02-02 20:17:03 -0300

    refactor(DXDguild): DXDguild inherits ERC20Guild and only adds vote function to voting machine as al

--- contracts/dxdao/DXDGuild.sol
+++ contracts/dxdao/DXDGuild.sol
@@ -2,8 +2,7 @@
 pragma solidity ^0.5.17;
 pragma experimental ABIEncoderV2;
 
-import "../erc20guild/ERC20GuildPermissioned.sol";
-import "../erc20guild/ERC20GuildSnapshot.sol";
+import "../erc20guild/ERC20Guild.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 
@@ -12,46 +11,34 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 /// @notice This smart contract has not be audited.
 /// An ERC20Guild that can only vote on a voting machine by calling the vote funtion
 /// and can be configures only by its owner.
-contract DXDGuild is ERC20GuildSnapshot, ERC20GuildPermissioned, Ownable {
+contract DXDGuild is ERC20Guild, Ownable {
 
-  constructor() public ERC20GuildSnapshot() ERC20GuildPermissioned() {}
+  constructor() public ERC20Guild() {}
     
     /// @dev Initilizer
     /// @param _token The address of the token to be used
     /// @param _proposalTime The minimun time for a proposal to be under votation
     /// @param _votesForExecution The token votes needed for a proposal to be executed
     /// @param _votesForCreation The minimum balance of tokens needed to create a proposal
+    /// @param _voteGas The gas to be used to calculate the vote gas refund
+    /// @param _maxGasPrice The maximum gas price to be refunded
+    /// @param _lockTime The minimum amount of seconds that the tokens would be locked
+
     /// @param votingMachine The voting machine where the guild will vote
     function initialize(
         address _token,
         uint256 _proposalTime,
         uint256 _votesForExecution,
         uint256 _votesForCreation,
+        uint256 _voteGas,
+        uint256 _maxGasPrice,
+        uint256 _lockTime,
         address votingMachine
     ) public {
-        super.initialize(_token, _proposalTime, _votesForExecution, _votesForCreation, "DXDGuild");
+        super.initialize(
+          _token, _proposalTime, _votesForExecution, _votesForCreation, "DXDGuild",  _voteGas, _maxGasPrice, _lockTime
+        );
         callPermissions[votingMachine][bytes4(keccak256("vote(bytes32,uint256,uint256,address)"))] = true;
-    }
-    
-    /// @dev Set the ERC20Guild configuration, can be called only executing a proposal 
-    /// or when it is initilized
-    /// @param _proposalTime The minimun time for a proposal to be under votation
-    /// @param _votesForExecution The token votes needed for a proposal to be executed
-    /// @param _votesForCreation The minimum balance of tokens needed to create a proposal
-    function setConfig(
-        uint256 _proposalTime,
-        uint256 _votesForExecution,
-        uint256 _votesForCreation
-    ) public {
-      require(
-          !initialized || (msg.sender == owner()), 
-          "ERC20Guild: Only callable by ERC20guild owner when initialized"
-      );
-      
-      initialized = true;
-      proposalTime = _proposalTime;
-      votesForExecution = _votesForExecution;
-      votesForCreation = _votesForCreation;
     }
 
 }
