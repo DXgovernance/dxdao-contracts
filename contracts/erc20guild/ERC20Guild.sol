1dc407bb233c4de68bee75d5115c37cce4846d13
AugustoL <me@augustol.com>
2021-02-02 20:15:58 -0300

    refactor(erc20guild): Merge ERC20GuildSnapshot into ERC20Guild contract

--- contracts/erc20guild/ERC20Guild.sol
+++ contracts/erc20guild/ERC20Guild.sol
@@ -7,6 +7,7 @@ import "openzeppelin-solidity/contracts/math/Math.sol";
 import "openzeppelin-solidity/contracts/cryptography/ECDSA.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "../utils/TokenVault.sol";
+import "../utils/Arrays.sol";
 
 /// @title ERC20Guild - DRAFT
 /// @author github:AugustoL
@@ -18,6 +19,8 @@ contract ERC20Guild {
     using SafeMath for uint256;
     using Math for uint256;
     using ECDSA for bytes32;
+    using SafeMath for uint256;
+    using Arrays for uint256[];
 
     IERC20 public token;
     bool public initialized = false;
@@ -55,6 +58,20 @@ contract ERC20Guild {
     }
 
     mapping(bytes32 => Proposal) public proposals;
+    
+    // Snapshotted values have arrays of ids and the value corresponding to that id. These could be an array of a
+    // Snapshot struct, but that would impede usage of functions that work on an array.
+    struct Snapshots {
+        uint256[] ids;
+        uint256[] values;
+    }
+    
+    mapping(bytes32 => uint256) public proposalSnapshots;
+    mapping (address => Snapshots) private _votesSnapshots;
+    Snapshots private _totalLockedSnapshots;
+
+    // Snapshot ids increase monotonically, with the first value being 1. An id of 0 is invalid.
+    uint256 private _currentSnapshotId;
     
     event ProposalCreated(bytes32 indexed proposalId);
     event ProposalExecuted(bytes32 indexed proposalId);
@@ -154,19 +171,15 @@ contract ERC20Guild {
         string memory description,
         bytes memory contentHash
     ) public isInitialized returns(bytes32) {
-        require(
-            votesOf(msg.sender) >= votesForCreation,
-            "ERC20Guild: Not enough tokens to create proposal"
-        );
+        require(votesOf(msg.sender) >= votesForCreation, "ERC20Guild: Not enough tokens to create proposal");
         require(
             (to.length == data.length) && (to.length == value.length),
             "ERC20Guild: Wrong length of to, data or value arrays"
         );
-        require(
-            to.length > 0,
-            "ERC20Guild: to, data value arrays cannot be empty"
-        );
+        require(to.length > 0, "ERC20Guild: to, data value arrays cannot be empty");
         bytes32 proposalId = keccak256(abi.encodePacked(msg.sender, now));
+        _currentSnapshotId = _currentSnapshotId.add(1);
+        proposalSnapshots[proposalId] = _currentSnapshotId;
         proposals[proposalId] = Proposal(
             msg.sender,
             now,
@@ -211,6 +224,10 @@ contract ERC20Guild {
     /// @param proposalId The id of the proposal to set the vote
     /// @param amount The amount of tokens to use as voting for the proposal
     function setVote(bytes32 proposalId, uint256 amount) public {
+        require(
+            votesOfAt(msg.sender, proposalSnapshots[proposalId]) >=  amount,
+            "ERC20Guild: Invalid amount"
+        );
         _setVote(msg.sender, proposalId, amount);
         _refundVote(msg.sender);
     }
@@ -261,6 +278,8 @@ contract ERC20Guild {
     /// @dev Lock tokens in the guild to be used as voting power
     /// @param amount The amount of tokens to be locked
     function lockTokens(uint256 amount) public {
+        _updateAccountSnapshot(msg.sender);
+        _updateTotalSupplySnapshot();
         tokenVault.deposit(msg.sender, amount);
         tokensLocked[msg.sender].amount = tokensLocked[msg.sender].amount.add(amount);
         tokensLocked[msg.sender].timestamp = block.timestamp.add(lockTime);
@@ -273,6 +292,8 @@ contract ERC20Guild {
     function releaseTokens(uint256 amount) public {
         require(votesOf(msg.sender) >= amount, "ERC20GuildLockable: Unable to release more tokens than locked");
         require(tokensLocked[msg.sender].timestamp < block.timestamp, "ERC20GuildLockable: Tokens still locked");
+        _updateAccountSnapshot(msg.sender);
+        _updateTotalSupplySnapshot();
         tokensLocked[msg.sender].amount = tokensLocked[msg.sender].amount.sub(amount);
         totalLocked = totalLocked.sub(amount);
         tokenVault.withdraw(msg.sender, amount);
@@ -359,6 +380,36 @@ contract ERC20Guild {
       }
       return votes;
     }
+    
+    /// @dev Get the voting power of an address at a certain snapshotId
+    /// @param account The address of the token account
+    /// @param snapshotId The snapshotId to be used
+    function votesOfAt(address account, uint256 snapshotId) public view returns (uint256) {
+        (bool snapshotted, uint256 value) = _valueAt(snapshotId, _votesSnapshots[account]);
+        if (snapshotted)
+            return value;
+        else 
+            return votesOf(account);
+    }
+    
+    /// @dev Get the voting power of multiple addressese at a certain snapshotId
+    /// @param accounts The addresses of the token accounts
+    /// @param snapshotIds The snapshotIds to be used
+    function votesOfAt(address[] memory accounts, uint256[] memory snapshotIds) public view returns(uint256[] memory) {
+        uint256[] memory votes = new uint256[](accounts.length);
+        for(uint i = 0; i < accounts.length; i ++)
+            votes[i] = votesOfAt(accounts[i], snapshotIds[i]);
+        return votes;
+    }
+
+    /// @dev Get the total amount of tokes locked at a certain snapshotId
+    function totalLockedAt(uint256 snapshotId) public view returns(uint256) {
+        (bool snapshotted, uint256 value) = _valueAt(snapshotId, _totalLockedSnapshots);
+        if (snapshotted)
+            return value;
+        else 
+            return totalLocked;
+    }
 
     /// @dev Get the information of a proposal
     /// @param proposalId The id of the proposal to get the information
@@ -431,6 +482,60 @@ contract ERC20Guild {
         uint256 amount
     ) public pure returns(bytes32) {
         return keccak256(abi.encodePacked(voter, proposalId, amount));
+    }
+    
+    function _valueAt(
+      uint256 snapshotId, Snapshots storage snapshots
+    ) private view returns (bool, uint256) {
+        require(snapshotId > 0, "ERC20Snapshot: id is 0");
+        // solhint-disable-next-line max-line-length
+        require(snapshotId <= _currentSnapshotId, "ERC20Snapshot: nonexistent id");
+
+        // When a valid snapshot is queried, there are three possibilities:
+        //  a) The queried value was not modified after the snapshot was taken. Therefore, a snapshot entry was never
+        //  created for this id, and all stored snapshot ids are smaller than the requested one. The value that corresponds
+        //  to this id is the current one.
+        //  b) The queried value was modified after the snapshot was taken. Therefore, there will be an entry with the
+        //  requested id, and its value is the one to return.
+        //  c) More snapshots were created after the requested one, and the queried value was later modified. There will be
+        //  no entry for the requested id: the value that corresponds to it is that of the smallest snapshot id that is
+        //  larger than the requested one.
+        //
+        // In summary, we need to find an element in an array, returning the index of the smallest value that is larger if
+        // it is not found, unless said value doesn't exist (e.g. when all values are smaller). Arrays.findUpperBound does
+        // exactly this.
+
+        uint256 index = snapshots.ids.findUpperBound(snapshotId);
+
+        if (index == snapshots.ids.length) {
+            return (false, 0);
+        } else {
+            return (true, snapshots.values[index]);
+        }
+    }
+
+    function _updateAccountSnapshot(address account) private {
+        _updateSnapshot(_votesSnapshots[account], votesOf(account));
+    }
+
+    function _updateTotalSupplySnapshot() private {
+        _updateSnapshot(_totalLockedSnapshots, totalLocked);
+    }
+
+    function _updateSnapshot(Snapshots storage snapshots, uint256 currentValue) private {
+        uint256 currentId = _currentSnapshotId;
+        if (_lastSnapshotId(snapshots.ids) < currentId) {
+            snapshots.ids.push(currentId);
+            snapshots.values.push(currentValue);
+        }
+    }
+
+    function _lastSnapshotId(uint256[] storage ids) private view returns (uint256) {
+        if (ids.length == 0) {
+            return 0;
+        } else {
+            return ids[ids.length - 1];
+        }
     }
 
 }
