6e43f6c3ba0042b01fbc8def0784a8cf34d7a132
AugustoL <me@augustol.com>
2021-02-11 21:08:31 -0300

    feat(dxdguild): Add function to create proposals based on a voting machine proposal

--- contracts/dxdao/DXDGuild.sol
+++ contracts/dxdao/DXDGuild.sol
@@ -15,6 +15,10 @@ contract DXDGuild is ERC20Guild, Ownable {
 
   constructor() public ERC20Guild() {}
     
+    address public votingMachine;
+    bytes4 public voteFuncSignature = bytes4(keccak256("vote(bytes32,uint256,uint256,address)"));
+    uint256 private _currentSnapshotId;
+
     /// @dev Initilizer
     /// @param _token The address of the token to be used
     /// @param _proposalTime The minimun time for a proposal to be under votation
@@ -24,7 +28,7 @@ contract DXDGuild is ERC20Guild, Ownable {
     /// @param _voteGas The gas to be used to calculate the vote gas refund
     /// @param _maxGasPrice The maximum gas price to be refunded
     /// @param _lockTime The minimum amount of seconds that the tokens would be locked
-    /// @param votingMachine The voting machine where the guild will vote
+    /// @param _votingMachine The voting machine where the guild will vote
     function initialize(
         address _token,
         uint256 _proposalTime,
@@ -34,7 +38,7 @@ contract DXDGuild is ERC20Guild, Ownable {
         uint256 _voteGas,
         uint256 _maxGasPrice,
         uint256 _lockTime,
-        address votingMachine
+        address _votingMachine
     ) public {
         super.initialize(
           _token,
@@ -47,7 +51,26 @@ contract DXDGuild is ERC20Guild, Ownable {
           _maxGasPrice,
           _lockTime
         );
-        callPermissions[votingMachine][bytes4(keccak256("vote(bytes32,uint256,uint256,address)"))] = true;
+        votingMachine = _votingMachine;
+        callPermissions[votingMachine][voteFuncSignature] = true;
+    }
+    
+    /// @dev Create a proposal to vote for yes or no on a proposal on a voting machine at the same time
+    /// @param votingMachineProposalId the proposalId of the voting machine
+    function createVotingMachineProposal(
+        bytes32 votingMachineProposalId
+    ) public isInitialized {
+      address[] memory _to = new address[](1);
+      _to[0] = votingMachine;
+      uint256[] memory _value = new uint256[](1);
+      _value[0] = 0;
+      bytes[] memory _data = new bytes[](1);
+      _data[0] = abi.encodeWithSelector(voteFuncSignature, votingMachineProposalId, 1, 0, address(this));
+      string memory _desc = "Positive vote";
+      createProposal(_to, _data, _value, _desc, abi.encodePacked(votingMachineProposalId));
+      _data[0] = abi.encodeWithSelector(voteFuncSignature, votingMachineProposalId, 2, 0, address(this));
+      _desc = "Negative vote";
+      createProposal(_to, _data, _value, _desc, abi.encodePacked(votingMachineProposalId));
     }
 
 }
