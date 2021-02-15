4d6b4da1b38002b9702262928ba962c24010670d
AugustoL <me@augustol.com>
2021-02-15 16:33:50 -0300

    feat(dxd-guild): keep track of proposals fro voting machine and only create/end them using dxdguild

--- contracts/dxdao/DXDGuild.sol
+++ contracts/dxdao/DXDGuild.sol
@@ -16,6 +16,14 @@ contract DXDGuild is ERC20Guild, Ownable {
     address public votingMachine;
     bytes4 public voteFuncSignature = bytes4(keccak256("vote(bytes32,uint256,uint256,address)"));
     uint256 private _currentSnapshotId;
+    
+    struct VotingMachineProposals {
+      bytes32 positiveProposal;
+      bytes32 negativeProposal;
+    }
+    
+    mapping(bytes32 => VotingMachineProposals) public votingMachineProposals;
+    mapping(bytes32 => bool) public proposalsForVotingMachine;
 
     /// @dev Initilizer
     /// @param _token The address of the token to be used
@@ -53,22 +61,87 @@ contract DXDGuild is ERC20Guild, Ownable {
         callPermissions[votingMachine][voteFuncSignature] = true;
     }
     
+    /// @dev Create a proposal with an static call data and extra information
+    /// @param to The receiver addresses of each call to be executed
+    /// @param data The data to be executed on each call to be executed
+    /// @param value The ETH value to be sent on each call to be executed
+    /// @param description A short description of the proposal
+    /// @param contentHash The content hash of the content reference of the proposal for the proposal to be executed
+    function createProposal(
+        address[] memory to,
+        bytes[] memory data,
+        uint256[] memory value,
+        string memory description,
+        bytes memory contentHash
+    ) public isInitialized returns(bytes32) {
+        require(votesOf(msg.sender) >= votesForCreation, "DXDGuild: Not enough tokens to create proposal");
+        require(
+            (to.length == data.length) && (to.length == value.length),
+            "DXDGuild: Wrong length of to, data or value arrays"
+        );
+        require(to.length > 0, "DXDGuild: to, data value arrays cannot be empty");
+        for (uint i = 0; i < to.length; i ++) {
+          require(
+            to[i] != votingMachine,
+            "DXDGuild: Use createVotingMachineProposal to submit proposals to voting machine"
+          );
+        }
+        return _createProposal(to, data, value, description, contentHash);
+    }
+    
+    /// @dev Execute a proposal that has already passed the votation time and has enough votes
+    /// @param proposalId The id of the proposal to be executed
+    function endProposal(bytes32 proposalId) public {
+      require(
+        !proposalsForVotingMachine[proposalId],
+        "DXDGuild: Use endVotingMachineProposal to end proposals to voting machine"
+      );
+      require(proposals[proposalId].state == ProposalState.Submitted, "ERC20Guild: Proposal already executed");
+      require(proposals[proposalId].endTime < now, "ERC20Guild: Proposal hasnt ended yet");
+      _endProposal(proposalId);
+    }
+    
     /// @dev Create a proposal to vote for yes or no on a proposal on a voting machine at the same time
     /// @param votingMachineProposalId the proposalId of the voting machine
     function createVotingMachineProposal(
         bytes32 votingMachineProposalId
     ) public isInitialized {
+      require(votesOf(msg.sender) >= votesForCreation, "DXDGuild: Not enough tokens to create proposal");      
       address[] memory _to = new address[](1);
       _to[0] = votingMachine;
-      uint256[] memory _value = new uint256[](1);
-      _value[0] = 0;
       bytes[] memory _data = new bytes[](1);
+      bytes memory _contentHash = abi.encodePacked(votingMachineProposalId);
       _data[0] = abi.encodeWithSelector(voteFuncSignature, votingMachineProposalId, 1, 0, address(this));
-      string memory _desc = "Positive vote";
-      createProposal(_to, _data, _value, _desc, abi.encodePacked(votingMachineProposalId));
+      votingMachineProposals[votingMachineProposalId].positiveProposal = 
+        _createProposal( _to, _data, new uint256[](1), string("Positive Vote"), _contentHash );
+      proposalsForVotingMachine[votingMachineProposals[votingMachineProposalId].positiveProposal] = true;
       _data[0] = abi.encodeWithSelector(voteFuncSignature, votingMachineProposalId, 2, 0, address(this));
-      _desc = "Negative vote";
-      createProposal(_to, _data, _value, _desc, abi.encodePacked(votingMachineProposalId));
+      votingMachineProposals[votingMachineProposalId].negativeProposal = 
+        _createProposal( _to, _data, new uint256[](1), string("Negative Vote"), _contentHash );
+      proposalsForVotingMachine[votingMachineProposals[votingMachineProposalId].negativeProposal] = true;
+    }
+    
+    /// @dev Ends proposals to vote for yes or no on a proposal on a voting machine at the same time
+    /// @param votingMachineProposalId the proposalId of the voting machine
+    function endVotingMachineProposal(
+        bytes32 votingMachineProposalId
+    ) public isInitialized {
+      Proposal storage positiveProposal = proposals[votingMachineProposals[votingMachineProposalId].positiveProposal];
+      Proposal storage negativeProposal = proposals[votingMachineProposals[votingMachineProposalId].negativeProposal];
+      require(positiveProposal.state == ProposalState.Submitted, "DXDGuild: Positive proposal already executed");
+      require(negativeProposal.state == ProposalState.Submitted, "DXDGuild: Negative proposal already executed");
+      require(positiveProposal.endTime < now, "DXDGuild: Positive proposal hasnt ended yet");
+      require(negativeProposal.endTime < now, "DXDGuild: Negative proposal hasnt ended yet");
+      
+      if (positiveProposal.totalVotes > negativeProposal.totalVotes) {
+        _endProposal(votingMachineProposals[votingMachineProposalId].positiveProposal);
+        negativeProposal.state = ProposalState.Rejected;
+        emit ProposalRejected(votingMachineProposals[votingMachineProposalId].negativeProposal);
+      } else {
+        _endProposal(votingMachineProposals[votingMachineProposalId].negativeProposal);
+        positiveProposal.state = ProposalState.Rejected;
+        emit ProposalRejected(votingMachineProposals[votingMachineProposalId].positiveProposal);
+      }
     }
 
 }
