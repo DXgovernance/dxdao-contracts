16389c48281210fc01bcceb8411df5e8b398076a
AugustoL <me@augustol.com>
2021-04-06 13:04:09 -0300

    refactor(scripts): Update OMN scripts used with latest deployment values and using OMN rinkeby multi

--- scripts/deploy-omn-guild.js
+++ scripts/deploy-omn-guild.js
@@ -4,28 +4,33 @@ const moment = require('moment');
 const web3 = hre.web3;
 
 const NULL_ADDRESS = "0x0000000000000000000000000000000000000000";
-const TOKEN_ADDRESS = "0xDcB2193aadCDfB9737C321011D991c35a77aaFad"
+const TOKEN_ADDRESS = "0x0A08ECa47C56C305F4FeB4fa062AEcd5807BeBb8"
+const PROXY_OWNER = "0x4F46d722d1699E9099e7597c80c78b7225E0100f";
+const MAX_INT = "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
 
 async function main() {
   const OMNGuild = await hre.ethers.getContractFactory("OMNGuild");
-  const omnToken = await hre.upgrades.deployProxy(OMNGuild, [
+  const omnGuild = await hre.upgrades.deployProxy(OMNGuild, [
       TOKEN_ADDRESS, // address token,
       moment.duration(7, 'days').asSeconds(), // uint256 _proposalTime,
       moment.duration(1, 'days').asSeconds(), // uint256 _timeForExecution,
-      2500, //uint256 _votesForExecution,
-      100, // uint256 _votesForCreation,
+      "10000", //uint256 _votesForExecution, 10%
+      web3.utils.toWei("100"), // uint256 _votesForCreation, 100 OMN tokens
       0, //uint256 _voteGas,
       0, // uint256 _maxGasPrice,
       moment.duration(2, 'months').asSeconds(), //uint256 _lockTime,
-      web3.utils.toWei("1000"), // uint256 _maxAmountVotes,
+      MAX_INT, // uint256 _maxAmountVotes,
       NULL_ADDRESS, // address _realityIO
     ], {initializer: "initialize(address,uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256,address)"}
   );
+  
+  const proxyAdmin = await hre.upgrades.admin.getInstance();
+  await proxyAdmin.changeProxyAdmin(omnGuild.address, PROXY_OWNER, { gasLimit: 1000000 });
+  await proxyAdmin.transferOwnership(PROXY_OWNER, { gasLimit: 1000000 });
 
-  await omnToken.deployed();
-  console.log("OMNGuild Proxy deployed to:", omnToken.address);
+  await omnGuild.deployed();
+  console.log("OMNGuild Proxy deployed to:", omnGuild.address);
   console.log("You can get the implementation address to validate from https://NETWORK.etherscan.io/proxyContractChecker?a=PROXY_ADDRESS")
-
 }
 
 main()
