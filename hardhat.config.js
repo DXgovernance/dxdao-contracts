7db1c4f96f2bcd131cd096aacd7d861dd4ada3af
AugustoL <me@augustol.com>
2021-05-26 11:33:51 -0300

    refactor(devop): Run linter, tests and coverage in one workflow

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,11 +11,67 @@ require('@openzeppelin/hardhat-upgrades');
 require("@nomiclabs/hardhat-etherscan");
 require('hardhat-dependency-compiler');
 
-
-
 const INFURA_PROJECT_ID = process.env.KEY_INFURA_API_KEY;
 const MNEMONIC = process.env.KEY_MNEMONIC;
 const ETHERSCAN_API_KEY = process.env.KEY_ETHERSCAN;
+
+const hardharNetworks = process.env.CI
+  ? {
+    hardhat: {
+      throwOnTransactionFailures: true,
+      throwOnCallFailures: true,
+      allowUnlimitedContractSize: true,
+      gasLimit: 9000000,
+      gasPrice: 10000000000, // 10 gwei
+      timeout: 60000
+    }
+  }
+  : {
+  hardhat: {
+    throwOnTransactionFailures: true,
+    throwOnCallFailures: true,
+    allowUnlimitedContractSize: true,
+    gasLimit: 9000000,
+    gasPrice: 10000000000, // 10 gwei
+    timeout: 60000
+  },
+  mainnet: {
+    url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
+    accounts: { mnemonic: MNEMONIC },
+    gasLimit: 9000000,
+    gasPrice: 100000000000, // 100 gwei
+    timeout: 60000
+  },
+  ropsten: {
+    url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
+    accounts: { mnemonic: MNEMONIC },
+    gasLimit: 9000000,
+    gasPrice: 1000000000, // 1 gwei
+    timeout: 60000
+  },
+  rinkeby: {
+    url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
+    accounts: { mnemonic: MNEMONIC },
+    gasLimit: 9000000,
+    gasPrice: 1000000000, // 1 gwei
+    timeout: 60000
+  },
+  kovan: {
+    url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
+    accounts: { mnemonic: MNEMONIC },
+    gasLimit: 9000000,
+    gasPrice: 10,
+    timeout: 60000
+  },
+  "arbitrum-testnet-v5": {
+    url: 'https://kovan5.arbitrum.io/rpc',
+    accounts: { mnemonic: MNEMONIC },
+    gasPrice: 0,
+    chainId: 144545313136048,
+    timeout: 60000
+  }
+};
+
 
 module.exports = {
   solidity: {
@@ -77,51 +133,7 @@ module.exports = {
     currency: 'USD',
     enabled: process.env.ENABLE_GAS_REPORTER === 'true'
   },
-  networks: {
-    hardhat: {
-      throwOnTransactionFailures: true,
-      throwOnCallFailures: true,
-      allowUnlimitedContractSize: true,
-      gasLimit: 9000000,
-      gasPrice: 10000000000, // 10 gwei
-      timeout: 60000
-    },
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: { mnemonic: MNEMONIC },
-      gasLimit: 9000000,
-      gasPrice: 100000000000, // 100 gwei
-      timeout: 60000
-    },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: { mnemonic: MNEMONIC },
-      gasLimit: 9000000,
-      gasPrice: 1000000000, // 1 gwei
-      timeout: 60000
-    },
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: { mnemonic: MNEMONIC },
-      gasLimit: 9000000,
-      gasPrice: 1000000000, // 1 gwei
-      timeout: 60000
-    },
-    kovan: {
-      url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: { mnemonic: MNEMONIC },
-      gasLimit: 9000000,
-      gasPrice: 10,
-      timeout: 60000
-    },
-    "arbitrum-testnet-v5": {
-      url: 'https://kovan5.arbitrum.io/rpc',
-      accounts: { mnemonic: MNEMONIC },
-      gasPrice: 0,
-      chainId: 144545313136048,
-      timeout: 60000
-    }
-  },
+  networks: hardharNetworks,
   etherscan: { apiKey: ETHERSCAN_API_KEY },
   dependencyCompiler: {
     paths: [
