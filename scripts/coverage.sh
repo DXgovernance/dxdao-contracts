6b9cc6db96bd71acce2fbee678b445b3f43f7d82
AugustoL <me@augustol.com>
2021-05-27 11:54:33 -0300

    fix(scripts/coverage): Dont end coverage script if error received, continue tests.

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-# Exit script as soon as a command fails.
-set -o errexit
-
 NODE_OPTIONS=--max-old-space-size=4096 OVERRIDE_GAS_LIMIT=0xfffffffffff OVERRIDE_GAS_PRICE=1 npx hardhat coverage
