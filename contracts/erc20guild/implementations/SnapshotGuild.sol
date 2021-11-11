6aaa819b4cbf4c73fe446956094e57f75522773d
AugustoL <me@augustol.com>
2021-11-11 14:44:43 -0300

    fix(scripts/solhint): FIx solhint script to parse erc20guild/implementations and fix warnings

--- contracts/erc20guild/implementations/SnapshotGuild.sol
+++ contracts/erc20guild/implementations/SnapshotGuild.sol
@@ -185,17 +185,17 @@ contract SnapshotGuild is LockableERC20Guild {
 
         // When a valid snapshot is queried, there are three possibilities:
         //  a) The queried value was not modified after the snapshot was taken. Therefore, a snapshot entry was never
-        //  created for this id, and all stored snapshot ids are smaller than the requested one. The value that corresponds
-        //  to this id is the current one.
+        //  created for this id, and all stored snapshot ids are smaller than the requested one. The value that
+        //  corresponds to this id is the current one.
         //  b) The queried value was modified after the snapshot was taken. Therefore, there will be an entry with the
         //  requested id, and its value is the one to return.
-        //  c) More snapshots were created after the requested one, and the queried value was later modified. There will be
-        //  no entry for the requested id: the value that corresponds to it is that of the smallest snapshot id that is
-        //  larger than the requested one.
+        //  c) More snapshots were created after the requested one, and the queried value was later modified. There will
+        //  be no entry for the requested id: the value that corresponds to it is that of the smallest snapshot id that
+        //  is larger than the requested one.
         //
-        // In summary, we need to find an element in an array, returning the index of the smallest value that is larger if
-        // it is not found, unless said value doesn't exist (e.g. when all values are smaller). Arrays.findUpperBound does
-        // exactly this.
+        // In summary, we need to find an element in an array, returning the index of the smallest value that is larger
+        // if it is not found, unless said value doesn't exist (e.g. when all values are smaller). Arrays.findUpperBound
+        // does exactly this.
 
         uint256 index = snapshots.ids.findUpperBound(snapshotId);
 
--- scripts/solhint.sh
+++ scripts/solhint.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 yarn solhint contracts/dxvote/*.sol 
-yarn solhint contracts/erc20guild/**.sol 
+yarn solhint contracts/erc20guild/*.sol 
+yarn solhint contracts/erc20guild/implementations/*.sol 
 yarn solhint contracts/omen/*.sol 
 yarn solhint contracts/dxdao/*.sol 
 
