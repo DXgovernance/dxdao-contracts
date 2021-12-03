e97ceab6f2b2130a2b0ebb9e9e82956bd88fc57e
AugustoL <me@augustol.com>
2021-12-03 14:27:27 -0300

    test(MigratableERC20Guild): Add tests showing hwo funds can be stealed with a malicious token vault

--- test/erc20guild/implementations/MigratableERC20Guild.js
+++ test/erc20guild/implementations/MigratableERC20Guild.js
@@ -23,6 +23,7 @@ const {
 const MigratableERC20Guild = artifacts.require("MigratableERC20Guild.sol");
 const GlobalPermissionRegistry = artifacts.require("GlobalPermissionRegistry.sol");
 const TokenVault = artifacts.require("TokenVault.sol");
+const TokenVaultThief = artifacts.require("TokenVaultThief.sol");
 
 require("chai").should();
 
@@ -176,6 +177,66 @@ contract("MigratableERC20Guild", function (accounts) {
       assert.equal(await erc20Guild.votingPowerOf(accounts[3]), "500000");
     });
 
+  });
+
+  it("Cant migrate to a invalid new vault", async function () {
+
+    tokenVaultB = await TokenVaultThief.new();
+    await tokenVaultB.initialize(guildTokenB.address, erc20Guild.address);
+
+    await guildTokenB.approve(tokenVaultB.address, 500000, { from: accounts[1] });
+    await guildTokenB.approve(tokenVaultB.address, 500000, { from: accounts[2] });
+    await erc20Guild.lockExternalTokens(500000, tokenVaultB.address, { from: accounts[1] });
+    await erc20Guild.lockExternalTokens(500000, tokenVaultB.address, { from: accounts[2] });
+    
+    const guildProposalId = await createProposal({
+      guild: erc20Guild,
+      actions: [{
+          to: [erc20Guild.address],
+          data: [
+            await new web3.eth.Contract(MigratableERC20Guild.abi)
+              .methods.changeTokenVault(tokenVaultB.address).encodeABI()
+          ],
+          value: [0],
+      }],
+      account: accounts[3],
+    });
+    await setAllVotesOnProposal({
+      guild: erc20Guild,
+      proposalId: guildProposalId,
+      action: 1,
+      account: accounts[3],
+    });
+    
+    const txVote = await setAllVotesOnProposal({
+      guild: erc20Guild,
+      proposalId: guildProposalId,
+      action: 1,
+      account: accounts[5],
+    });
+    
+    assert.equal(await erc20Guild.votingPowerOf(accounts[1]), "50000");
+    assert.equal(await erc20Guild.votingPowerOf(accounts[2]), "50000");
+
+    await time.increase(time.duration.seconds(31));
+    const receipt = await erc20Guild.endProposal(guildProposalId);
+    expectEvent(receipt, "ProposalStateChanged", {
+      proposalId: guildProposalId,
+      newState: "3"
+    });
+
+    assert.equal(await erc20Guild.votingPowerOf(accounts[1]), "500000");
+    assert.equal(await erc20Guild.votingPowerOf(accounts[2]), "500000");
+    assert.equal(await erc20Guild.votingPowerOf(accounts[3]), "0");
+    assert.equal(await erc20Guild.votingPowerOf(accounts[4]), "0");
+    assert.equal(await erc20Guild.votingPowerOf(accounts[5]), "0");
+
+    assert.equal(await erc20Guild.getToken(), guildTokenB.address);
+    assert.equal(await erc20Guild.getTokenVault(), tokenVaultB.address);
+
+    await guildTokenB.approve(tokenVaultB.address, 500000, { from: accounts[3] });
+    await erc20Guild.lockTokens(500000, { from: accounts[3] });
+    assert.equal(await erc20Guild.votingPowerOf(accounts[3]), "500000");
   });
 
 });
