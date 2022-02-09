8c60a2d4b08bf8f68d29b42430342d4603192045
rossneilson <rossneilson99@gmail.com>
2022-02-09 10:31:45 +0000

    test: Adding tests for release and revoke in vesting contract

--- test/dxvote/DXDVestingFactory.js
+++ test/dxvote/DXDVestingFactory.js
@@ -18,31 +18,51 @@ const ERC20Mock = artifacts.require("ERC20Mock.sol");
 require("chai").should();
 
 contract("DXDVestingFactory", function (accounts) {
+  const dao = accounts[0];
+  const contributor = accounts[1];
+  let dxdTokenMock, vestingFactory;
 
-  describe("Create", function () {
+  describe("Create Vesting Contracts", function () {
+    beforeEach(async function () {
+      dxdTokenMock = await ERC20Mock.new(dao, 1000);
+      vestingFactory = await DXDVestingFactory.new(dxdTokenMock.address, dao);
+    });
 
-    it("Can create vesting and transfer ownership", async function () {
-      const dao = accounts[0];
-      const contributor = accounts[1];
+    it("Can transfer ownership", async function () {
+      await dxdTokenMock.approve(vestingFactory.address, 1, {
+        from: dao,
+      });
 
-      const dxdTokenMock = await ERC20Mock.new(dao, 1000);
-
-      const vestingFactory = await DXDVestingFactory.new(
-        dxdTokenMock.address,
-        dao
+      const timeNow = (await time.latest()).toNumber();
+      const receipt = await vestingFactory.create(
+        contributor,
+        timeNow,
+        60000,
+        1200000,
+        1,
+        { from: dao }
       );
+      const newVestingContract = await new web3.eth.Contract(
+        TokenVesting.abi,
+        receipt.logs[0].args.vestingContractAddress
+      );
+
+      const owner = await newVestingContract.methods.owner().call();
+      expect(owner).to.be.equal(dao);
+    });
+
+    it("Can be redeemed by contributor after cliff", async function () {
       await dxdTokenMock.approve(vestingFactory.address, 10, {
         from: dao,
       });
 
       const timeNow = (await time.latest()).toNumber();
-      
       const receipt = await vestingFactory.create(
         contributor,
         timeNow,
-        600,
-        1200,
-        1,
+        60,
+        120,
+        10,
         { from: dao }
       );
 
@@ -51,8 +71,80 @@ contract("DXDVestingFactory", function (accounts) {
         receipt.logs[0].args.vestingContractAddress
       );
 
-      const owner = await newVestingContract.methods.owner().call();
-      expect(owner).to.be.equal(dao);
+      await time.increase(time.duration.seconds(60));
+      await newVestingContract.methods.release(dxdTokenMock.address).send({
+        from: contributor,
+      });
+
+      const balance = await dxdTokenMock.balanceOf(contributor);
+      expect(balance.toNumber()).to.be.equal(5);
+    });
+
+    it("Can be redeemed by contributor end", async function () {
+      await dxdTokenMock.approve(vestingFactory.address, 10, {
+        from: dao,
+      });
+
+      const timeNow = (await time.latest()).toNumber();
+      const receipt = await vestingFactory.create(
+        contributor,
+        timeNow,
+        60,
+        120,
+        10,
+        { from: dao }
+      );
+
+      const newVestingContract = await new web3.eth.Contract(
+        TokenVesting.abi,
+        receipt.logs[0].args.vestingContractAddress
+      );
+
+      await time.increase(time.duration.seconds(120));
+
+      await newVestingContract.methods.release(dxdTokenMock.address).send({
+        from: contributor,
+      });
+      const balance = await dxdTokenMock.balanceOf(contributor);
+      expect(balance.toNumber()).to.be.equal(10);
+    });
+
+    it("Can be revoked by the dao", async function () {
+      await dxdTokenMock.approve(vestingFactory.address, 10, {
+        from: dao,
+      });
+
+      const timeNow = (await time.latest()).toNumber();
+      const receipt = await vestingFactory.create(
+        contributor,
+        timeNow,
+        60,
+        120,
+        10,
+        { from: dao }
+      );
+
+      const newVestingContract = await new web3.eth.Contract(
+        TokenVesting.abi,
+        receipt.logs[0].args.vestingContractAddress
+      );
+
+      await time.increase(time.duration.seconds(60));
+      await newVestingContract.methods.revoke(dxdTokenMock.address).send({
+        from: dao,
+      });
+
+      await time.increase(time.duration.seconds(60));
+      await newVestingContract.methods.release(dxdTokenMock.address).send({
+        from: contributor,
+      });
+
+      await expectRevert(
+        newVestingContract.methods
+          .release(dxdTokenMock.address)
+          .send({ from: contributor }),
+        "TokenVesting: no tokens are due"
+      );
     });
   });
 });
