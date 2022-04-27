b25149b2a1f11d44f3a01cf166291b45e8e3872e
AugustoL <augustolemble@pm.me>
2022-04-27 15:46:20 -0300

    fix(tests): increase test timeout to 300 seconds for dxvote deploy

--- test/dxvote/deploy.js
+++ test/dxvote/deploy.js
@@ -2,6 +2,7 @@ require("@nomiclabs/hardhat-web3");
 
 contract("DXvote develop deployment", function () {
   it("Deploy DXvote", async function () {
+    this.timeout(300000);
     await hre.run("deploy-dxvote-develop");
   });
 });
