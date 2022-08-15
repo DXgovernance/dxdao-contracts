fda032489e042423fe197904d0e2fc0d78d5e86c
Dino <crescimbenidino@gmail.com>
2022-08-15 12:33:50 -0300

    feat(DXDVotingMachine): copied GenesisProtocol code into DXDVotingMachine file

--- contracts/dxvote/DXDVotingMachine.sol
+++ contracts/dxvote/DXDVotingMachine.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.5.11;
 
-import "../daostack/votingMachines/GenesisProtocol.sol";
+import "openzeppelin-solidity/contracts/cryptography/ECDSA.sol";
+import "../daostack/votingMachines/GenesisProtocolLogic.sol";
 
 /**
  * @title GenesisProtocol implementation designed for DXdao
@@ -12,8 +13,24 @@ import "../daostack/votingMachines/GenesisProtocol.sol";
  *  - Signal Votes: Voters can signal their decisions with near 50k gas, the signaled votes can be executed on
  *    chain by anyone.
  */
-contract DXDVotingMachine is GenesisProtocol {
+contract DXDVotingMachine is IntVoteInterface, GenesisProtocolLogic {
     uint256 private constant MAX_BOOSTED_PROPOSALS = 4096;
+    using ECDSA for bytes32;
+
+    // Digest describing the data the user signs according EIP 712.
+    // Needs to match what is passed to Metamask.
+    bytes32 public constant DELEGATION_HASH_EIP712 =
+        keccak256(
+            abi.encodePacked(
+                "address GenesisProtocolAddress",
+                "bytes32 ProposalId",
+                "uint256 Vote",
+                "uint256 AmountToStake",
+                "uint256 Nonce"
+            )
+        );
+
+    mapping(address => uint256) public stakesNonce; //stakes Nonce
 
     // organization id scheme => parameters hash => required % of votes in boosted proposal.
     // 100 == 1%, 2500 == 25%.
@@ -58,7 +75,7 @@ contract DXDVotingMachine is GenesisProtocol {
     /**
      * @dev Constructor
      */
-    constructor(IERC20 _stakingToken) public GenesisProtocol(_stakingToken) {
+    constructor(IERC20 _stakingToken) public GenesisProtocolLogic(_stakingToken) {
         require(address(_stakingToken) != address(0), "wrong _stakingToken");
         stakingToken = _stakingToken;
     }
@@ -72,6 +89,65 @@ contract DXDVotingMachine is GenesisProtocol {
             "DXDVotingMachine: Address not registered in organizationRefounds"
         );
         organizationRefunds[msg.sender].balance = organizationRefunds[msg.sender].balance.add(msg.value);
+    }
+
+    /**
+     * @dev staking function
+     * @param _proposalId id of the proposal
+     * @param _vote  NO(2) or YES(1).
+     * @param _amount the betting amount
+     * @return bool true - the proposal has been executed
+     *              false - otherwise.
+     */
+    function stake(
+        bytes32 _proposalId,
+        uint256 _vote,
+        uint256 _amount
+    ) external returns (bool) {
+        return _stake(_proposalId, _vote, _amount, msg.sender);
+    }
+
+    /**
+     * @dev stakeWithSignature function
+     * @param _proposalId id of the proposal
+     * @param _vote  NO(2) or YES(1).
+     * @param _amount the betting amount
+     * @param _nonce nonce value ,it is part of the signature to ensure that
+              a signature can be received only once.
+     * @param _signatureType signature type
+              1 - for web3.eth.sign
+              2 - for eth_signTypedData according to EIP #712.
+     * @param _signature  - signed data by the staker
+     * @return bool true - the proposal has been executed
+     *              false - otherwise.
+     */
+    function stakeWithSignature(
+        bytes32 _proposalId,
+        uint256 _vote,
+        uint256 _amount,
+        uint256 _nonce,
+        uint256 _signatureType,
+        bytes calldata _signature
+    ) external returns (bool) {
+        // Recreate the digest the user signed
+        bytes32 delegationDigest;
+        if (_signatureType == 2) {
+            delegationDigest = keccak256(
+                abi.encodePacked(
+                    DELEGATION_HASH_EIP712,
+                    keccak256(abi.encodePacked(address(this), _proposalId, _vote, _amount, _nonce))
+                )
+            );
+        } else {
+            delegationDigest = keccak256(abi.encodePacked(address(this), _proposalId, _vote, _amount, _nonce))
+                .toEthSignedMessageHash();
+        }
+        address staker = delegationDigest.recover(_signature);
+        //a garbage staker address due to wrong signature will revert due to lack of approval and funds.
+        require(staker != address(0), "staker address cannot be 0");
+        require(stakesNonce[staker] == _nonce);
+        stakesNonce[staker] = stakesNonce[staker].add(1);
+        return _stake(_proposalId, _vote, _amount, staker);
     }
 
     /**
@@ -142,6 +218,66 @@ contract DXDVotingMachine is GenesisProtocol {
         bool voteResult = internalVote(_proposalId, voter, _vote, _amount);
         _refundVote(proposal.organizationId);
         return voteResult;
+    }
+
+    /**
+     * @dev Cancel the vote of the msg.sender.
+     * cancel vote is not allow in genesisProtocol so this function doing nothing.
+     * This function is here in order to comply to the IntVoteInterface .
+     */
+    function cancelVote(bytes32 _proposalId) external votable(_proposalId) {
+        //this is not allowed
+        return;
+    }
+
+    /**
+     * @dev execute check if the proposal has been decided, and if so, execute the proposal
+     * @param _proposalId the id of the proposal
+     * @return bool true - the proposal has been executed
+     *              false - otherwise.
+     */
+    function execute(bytes32 _proposalId) external votable(_proposalId) returns (bool) {
+        return _execute(_proposalId);
+    }
+
+    /**
+     * @dev getProposalTimes returns proposals times variables.
+     * @param _proposalId id of the proposal
+     * @return proposals times array
+     */
+    function getProposalTimes(bytes32 _proposalId) external view returns (uint256[3] memory times) {
+        return proposals[_proposalId].times;
+    }
+
+    /**
+     * @dev voteInfo returns the vote and the amount of reputation of the user committed to this proposal
+     * @param _proposalId the ID of the proposal
+     * @param _voter the address of the voter
+     * @return uint256 vote - the voters vote
+     *        uint256 reputation - amount of reputation committed by _voter to _proposalId
+     */
+    function voteInfo(bytes32 _proposalId, address _voter) external view returns (uint256, uint256) {
+        Voter memory voter = proposals[_proposalId].voters[_voter];
+        return (voter.vote, voter.reputation);
+    }
+
+    /**
+     * @dev voteStatus returns the reputation voted for a proposal for a specific voting choice.
+     * @param _proposalId the ID of the proposal
+     * @param _choice the index in the
+     * @return voted reputation for the given choice
+     */
+    function voteStatus(bytes32 _proposalId, uint256 _choice) external view returns (uint256) {
+        return proposals[_proposalId].votes[_choice];
+    }
+
+    /**
+     * @dev isVotable check if the proposal is votable
+     * @param _proposalId the ID of the proposal
+     * @return bool true or false
+     */
+    function isVotable(bytes32 _proposalId) external view returns (bool) {
+        return _isVotable(_proposalId);
     }
 
     /**
@@ -491,6 +627,32 @@ contract DXDVotingMachine is GenesisProtocol {
     }
 
     /**
+     * @dev proposalStatus return the total votes and stakes for a given proposal
+     * @param _proposalId the ID of the proposal
+     * @return uint256 preBoostedVotes YES
+     * @return uint256 preBoostedVotes NO
+     * @return uint256 total stakes YES
+     * @return uint256 total stakes NO
+     */
+    function proposalStatus(bytes32 _proposalId)
+        external
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256,
+            uint256
+        )
+    {
+        return (
+            proposals[_proposalId].preBoostedVotes[YES],
+            proposals[_proposalId].preBoostedVotes[NO],
+            proposals[_proposalId].stakes[YES],
+            proposals[_proposalId].stakes[NO]
+        );
+    }
+
+    /**
      * @dev proposalStatusWithVotes return the total votes, preBoostedVotes and stakes for a given proposal
      * @param _proposalId the ID of the proposal
      * @return uint256 votes YES
@@ -520,6 +682,80 @@ contract DXDVotingMachine is GenesisProtocol {
             proposals[_proposalId].stakes[YES],
             proposals[_proposalId].stakes[NO]
         );
+    }
+
+    /**
+     * @dev getProposalOrganization return the organizationId for a given proposal
+     * @param _proposalId the ID of the proposal
+     * @return bytes32 organization identifier
+     */
+    function getProposalOrganization(bytes32 _proposalId) external view returns (bytes32) {
+        return (proposals[_proposalId].organizationId);
+    }
+
+    /**
+     * @dev getStaker return the vote and stake amount for a given proposal and staker
+     * @param _proposalId the ID of the proposal
+     * @param _staker staker address
+     * @return uint256 vote
+     * @return uint256 amount
+     */
+    function getStaker(bytes32 _proposalId, address _staker) external view returns (uint256, uint256) {
+        return (proposals[_proposalId].stakers[_staker].vote, proposals[_proposalId].stakers[_staker].amount);
+    }
+
+    /**
+     * @dev voteStake return the amount stakes for a given proposal and vote
+     * @param _proposalId the ID of the proposal
+     * @param _vote vote number
+     * @return uint256 stake amount
+     */
+    function voteStake(bytes32 _proposalId, uint256 _vote) external view returns (uint256) {
+        return proposals[_proposalId].stakes[_vote];
+    }
+
+    /**
+     * @dev winningVote return the winningVote for a given proposal
+     * @param _proposalId the ID of the proposal
+     * @return uint256 winningVote
+     */
+    function winningVote(bytes32 _proposalId) external view returns (uint256) {
+        return proposals[_proposalId].winningVote;
+    }
+
+    /**
+     * @dev state return the state for a given proposal
+     * @param _proposalId the ID of the proposal
+     * @return ProposalState proposal state
+     */
+    function state(bytes32 _proposalId) external view returns (ProposalState) {
+        return proposals[_proposalId].state;
+    }
+
+    /**
+     * @dev isAbstainAllow returns if the voting machine allow abstain (0)
+     * @return bool true or false
+     */
+    function isAbstainAllow() external pure returns (bool) {
+        return false;
+    }
+
+    /**
+     * @dev getAllowedRangeOfChoices returns the allowed range of choices for a voting machine.
+     * @return min - minimum number of choices
+               max - maximum number of choices
+     */
+    function getAllowedRangeOfChoices() external pure returns (uint256 min, uint256 max) {
+        return (YES, NO);
+    }
+
+    /**
+     * @dev score return the proposal score
+     * @param _proposalId the ID of the proposal
+     * @return uint256 proposal score.
+     */
+    function score(bytes32 _proposalId) public view returns (uint256) {
+        return _score(_proposalId);
     }
 
     /**
