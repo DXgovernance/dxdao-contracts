fd5c688cd83e1097a339253096fd4704ef1ce152
Kenny <59825171+Kenny-Gin1@users.noreply.github.com>
2022-08-16 15:11:40 +0200

    chore: compiles correctly in 0.8.8 solidity

--- contracts/dxdao/DxReputation.sol
+++ contracts/dxdao/DxReputation.sol
@@ -1,8 +1,9 @@
+// SPDX-License-Identifier: AGPL-3.0
 pragma solidity 0.8.8;
 
-import "@openzeppelin/contracts-upgradeable/ownership/Ownable.sol";
-import "@openzeppelin/contracts-upgradeable/contracts/token/ERC20/extensions/ERC29SnapshotUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
+import "@openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/ERC20SnapshotUpgradeable.sol";
+import "@openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
 
 /**
  * @title Reputation system
@@ -12,27 +13,26 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
  * The Reputation contract maintain a map of address to reputation value.
  * It provides an onlyOwner functions to mint and burn reputation _to (or _from) a specific address.
  */
-contract Reputation is Ownable, Initializable, ERC20SnapshotUpgradeable {
+contract Reputation is Initializable, OwnableUpgradeable, ERC20SnapshotUpgradeable {
     event Mint(address indexed _to, uint256 _amount);
-    // Event indicating burning of reputation for an address.
     event Burn(address indexed _from, uint256 _amount);
 
     function initialize(string memory name, string memory symbol) public initializer {
-        _ERC20_init(name, symbol);
-        _Ownable_init();
+        __ERC20_init(name, symbol);
+        __Ownable_init();
     }
 
-    function mint(address memory _user, uint256 memory _amount) public onlyOwner returns (bool) {
-        _mint(_user, _amount);
-        _snapshot();
-        emit Mint(_to, _amount);
-        return true;
-    }
-
-    // @notice Generates `_amount` reputation that are assigned to `_owner`
+    // @notice Generates `_amount` reputation that are assigned to `_user`
     // @param _user The address that will be assigned the new reputation
     // @param _amount The quantity of reputation generated
     // @return True if the reputation are generated correctly
+    function mint(address _user, uint256 _amount) public onlyOwner returns (bool) {
+        _mint(_user, _amount);
+        _snapshot();
+        emit Mint(_user, _amount);
+        return true;
+    }
+
     function mintMultiple(address[] memory _user, uint256[] memory _amount) public onlyOwner returns (bool) {
         for (uint256 i = 0; i < _user.length; i++) {
             _mint(_user[i], _amount[i]);
@@ -42,22 +42,22 @@ contract Reputation is Ownable, Initializable, ERC20SnapshotUpgradeable {
         return true;
     }
 
-    // @notice Burns `_amount` reputation from `_owner`
+    // @notice Burns `_amount` reputation from `_user`
     // @param _user The address that will lose the reputation
     // @param _amount The quantity of reputation to burn
     // @return True if the reputation are burned correctly
     function burn(address _user, uint256 _amount) public onlyOwner returns (bool) {
         _burn(_user, _amount);
         _snapshot();
-        emit Burn(_user, amount);
+        emit Burn(_user, _amount);
         return true;
     }
 
-    function burnMultiplie(address[] memory _user, uint256 _amount) public onlyOwner returns (bool) {
+    function burnMultiple(address[] memory _user, uint256 _amount) public onlyOwner returns (bool) {
         for (uint256 i = 0; i < _user.length; i++) {
             _burn(_user[i], _amount);
             _snapshot();
-            emit Burn(_user[i], amount);
+            emit Burn(_user[i], _amount);
         }
         return true;
     }
