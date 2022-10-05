8224ce506ea232e827525dc18f8bc170a8fd28b1
AugustoL <augustolemble@pm.me>
2022-10-05 16:53:44 -0500

    feat(contracts/deploy): add deploy contracts for guilds and nanodeployer

---
+++ contracts/deploy/DXdaoDevopsGuildDeployer.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.8;
+
+import "../erc20guild/implementations/SnapshotRepERC20Guild.sol";
+import "../utils/ERC20/ERC20SnapshotRep.sol";
+import "../utils/PermissionRegistry.sol";
+
+contract DXdaoDevopsGuildDeployer {
+    constructor() {
+        PermissionRegistry permissionRegistry = new PermissionRegistry();
+        permissionRegistry.initialize();
+        ERC20SnapshotRep repToken = new ERC20SnapshotRep();
+        repToken.initialize("DXdao Devops REP", "DREP");
+        SnapshotRepERC20Guild newGuild = new SnapshotRepERC20Guild();
+        newGuild.initialize(
+            address(repToken),
+            3 days, // proposal time
+            6 hours, // time for execution
+            5000, // 50% voting power for proposal execution
+            500, // 5% voting power for proposal creation
+            "DXdao Devops Guild", // guild name
+            0, // vote gas
+            0, // max gas price
+            10, // max active proposals
+            3 days, // lock time, not used
+            address(permissionRegistry)
+        );
+        permissionRegistry.setETHPermissionDelay(address(newGuild), 1);
+    }
+}
