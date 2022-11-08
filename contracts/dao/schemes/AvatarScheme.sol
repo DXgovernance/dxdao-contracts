651c9722fa52a299f65afa174d29d2e3bc3d6e02
Dino <crescimbenidino@gmail.com>
2022-11-08 09:41:25 -0300

    refactor(AvatarScheme): removed SafeMath

--- contracts/dao/schemes/AvatarScheme.sol
+++ contracts/dao/schemes/AvatarScheme.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "./Scheme.sol";
 
@@ -17,7 +16,6 @@ import "./Scheme.sol";
  * sender.
  */
 contract AvatarScheme is Scheme {
-    using SafeMath for uint256;
     using Address for address;
 
     /**
@@ -57,7 +55,7 @@ contract AvatarScheme is Scheme {
         Proposal storage proposal = proposals[_proposalId];
         require(proposal.state == ProposalState.Submitted, "AvatarScheme: must be a submitted proposal");
 
-        if (proposal.submittedTime.add(maxSecondsForExecution) < block.timestamp) {
+        if (proposal.submittedTime + maxSecondsForExecution < block.timestamp) {
             // If the amount of time passed since submission plus max proposal time is lower than block timestamp
             // the proposal timeout execution is reached and proposal cant be executed from now on
 
@@ -124,9 +122,8 @@ contract AvatarScheme is Scheme {
             }
             // Cant mint or burn more REP than the allowed percentaged set in the wallet scheme initialization
             require(
-                (oldRepSupply.mul(uint256(100).add(maxRepPercentageChange)).div(100) >=
-                    getNativeReputationTotalSupply()) &&
-                    (oldRepSupply.mul(uint256(100).sub(maxRepPercentageChange)).div(100) <=
+                ((oldRepSupply * (uint256(100) + maxRepPercentageChange)) / 100 >= getNativeReputationTotalSupply()) &&
+                    ((oldRepSupply * (uint256(100) - maxRepPercentageChange)) / 100 <=
                         getNativeReputationTotalSupply()),
                 "AvatarScheme: maxRepPercentageChange passed"
             );
