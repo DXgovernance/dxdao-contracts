2d2edf713f8c6c11e64da5a76bbb57de3b99f364
Milton Tulli <milton.tulli@gmail.com>
2023-01-24 16:38:18 -0300

    DAOReputation: refactor to include VPToken callback function on mint and burn

--- contracts/dao/DAOReputation.sol
+++ contracts/dao/DAOReputation.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.17;
 
 import "../utils/ERC20/ERC20SnapshotRep.sol";
+import "../dao/VotingPowerToken.sol";
 
 /**
  * @title DAO Reputation
@@ -11,5 +12,102 @@ import "../utils/ERC20/ERC20SnapshotRep.sol";
  * each modification of the supply of the token (every mint an burn).
  */
 contract DAOReputation is ERC20SnapshotRep {
+    /// @notice Voting Power Token address
+    address public votingPowerToken;
 
+    /// @notice Emitted if controller address is zero
+    error DAOReputation__VPTokenAddresCannotBeZero();
+
+    function initialize(string memory name, string memory symbol, address _votingPowerToken) external initializer {
+        __ERC20_init(name, symbol);
+        __Ownable_init();
+        votingPowerToken = _votingPowerToken;
+    }
+
+    /**
+     * @dev Generates `amount` reputation that are assigned to `account`
+     * @param account The address that will be assigned the new reputation
+     * @param amount The quantity of reputation generated
+     * @return success True if the reputation are generated correctly
+     */
+    function mint(
+        address account,
+        uint256 amount
+    ) external override(ERC20SnapshotRep) onlyOwner returns (bool success) {
+        _addHolder(account);
+        _mint(account, amount);
+        _snapshot();
+        emit Mint(account, amount);
+        VotingPowerToken(votingPowerToken).callback(account);
+        return true;
+    }
+
+    /**
+     * @dev Mint reputation for multiple accounts
+     * @param accounts The accounts that will be assigned the new reputation
+     * @param amount The quantity of reputation generated for each account
+     * @return success True if the reputation are generated correctly
+     */
+    function mintMultiple(
+        address[] memory accounts,
+        uint256[] memory amount
+    ) external override(ERC20SnapshotRep) onlyOwner returns (bool success) {
+        for (uint256 i = 0; i < accounts.length; i++) {
+            _addHolder(accounts[i]);
+            _mint(accounts[i], amount[i]);
+            _snapshot();
+            emit Mint(accounts[i], amount[i]);
+            VotingPowerToken(votingPowerToken).callback(account);
+        }
+        return true;
+    }
+
+    /**
+     * @dev Burns ` amount` reputation from ` account`
+     * @param  account The address that will lose the reputation
+     * @param  amount The quantity of reputation to burn
+     * @return success True if the reputation are burned correctly
+     */
+    function burn(
+        address account,
+        uint256 amount
+    ) external override(ERC20SnapshotRep) onlyOwner returns (bool success) {
+        _burn(account, amount);
+        _removeHolder(account);
+        _snapshot();
+        emit Burn(account, amount);
+        VotingPowerToken(votingPowerToken).callback(account);
+        return true;
+    }
+
+    /**
+     * @dev Burn reputation from multiple accounts
+     * @param  accounts The accounts that will lose the reputation
+     * @param  amount The quantity of reputation to burn for each account
+     * @return success True if the reputation are generated correctly
+     */
+    function burnMultiple(
+        address[] memory accounts,
+        uint256[] memory amount
+    ) external override(ERC20SnapshotRep) onlyOwner returns (bool success) {
+        for (uint256 i = 0; i < accounts.length; i++) {
+            _burn(accounts[i], amount[i]);
+            _removeHolder(accounts[i]);
+            _snapshot();
+            emit Burn(accounts[i], amount[i]);
+            VotingPowerToken(votingPowerToken).callback(account);
+        }
+        return true;
+    }
+
+    /**
+     * @dev Sets new Voting Power Token contract address
+     * @param _votingPowerToken The address of the new VPToken contract
+     */
+    function setVotingPowerTokenAddress(address _votingPowerToken) external onlyOwner {
+        if (_votingPowerToken == address(0)) {
+            revert DAOReputation__VPTokenAddresCannotBeZero();
+        }
+        votingPowerToken = _votingPowerToken;
+    }
 }
