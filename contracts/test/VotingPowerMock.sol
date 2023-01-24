0085e8e82e9615ecf6525ae25418bf8960bbc4c1
fnanni-0 <fnanni@itba.edu.ar>
2023-01-24 18:47:55 -0300

    test: mock voting power contract

---
+++ contracts/test/VotingPowerMock.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.8.17;
+
+// mock class using ERC20
+contract VotingPowerMock {
+    constructor() {}
+
+    function callback(address account) external {}
+}
