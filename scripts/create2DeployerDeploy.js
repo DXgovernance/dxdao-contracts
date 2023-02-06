0d061e1495d2291b60d00f2bd8b0e3ce47b5f8f8
AugustoL <augustolemble@pm.me>
2023-02-06 13:01:42 -0300

    refactor(scripts/create2deployer): add deployer account and gas cost

--- scripts/create2DeployerDeploy.js
+++ scripts/create2DeployerDeploy.js
@@ -2,21 +2,22 @@ require("@nomiclabs/hardhat-web3");
 
 task("create2DeployerDeploy", "Deploy a Create2Deployer").setAction(
   async () => {
-    const create2PrivateDeployer = await hre.artifacts.require(
-      "Create2Deployer"
-    );
+    const Create2Deployer = await hre.artifacts.require("Create2Deployer");
     const web3 = hre.web3;
     const gasPrice = 1000000000 * 100;
     const gasAmount = 3000000;
 
     const deployResult = await hre.run("keylessDeploy", {
-      bytecode: create2PrivateDeployer.bytecode,
+      bytecode: Create2Deployer.bytecode,
       signaturevalue:
         "1820182018201820182018201820182018201820182018201820182018201820",
       gas: gasAmount,
       gasprice: gasPrice,
       execute: false,
     });
+
+    // Gast cost: 0.3 ETH
+    // keyless deployer account: 0x669082Ee2F478e0cb06a532ea859Bd81AC032cAA
 
     const sender = (await web3.eth.getAccounts())[0];
 
@@ -28,7 +29,7 @@ task("create2DeployerDeploy", "Deploy a Create2Deployer").setAction(
       });
 
     return await hre.run("keylessDeploy", {
-      bytecode: create2PrivateDeployer.bytecode,
+      bytecode: Create2Deployer.bytecode,
       signaturevalue:
         "1820182018201820182018201820182018201820182018201820182018201820",
       gas: gasAmount,
